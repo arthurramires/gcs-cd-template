f74da079ab7c7a9e2ec5a30094c8ee59d52099ba
Arthur Ramires <52502727+arthurramires@users.noreply.github.com>
2025-06-24 18:11:39 +0000

    feat: criando estrutura inicial do projeto

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import './style.css'
+
+document.querySelector('#app').innerHTML = `
+  <h1>Laboratório de Entrega Contínua (CD)</h1>
+  <p class="read-the-docs">
+    Este é o nosso projeto inicial. O objetivo é automatizar o deploy!
+  </p>
+`
